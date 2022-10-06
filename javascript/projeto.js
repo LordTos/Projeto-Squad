8193e0b692635a7d838c7ecedbb5d408d94b927a
Thiago de Oliveira Sant' Anna <113534772+LordTos@users.noreply.github.com>
2022-10-06 14:54:43 -0300

    Add files via upload

---
+++ javascript/projeto.js
@@ -0,0 +1,10 @@
+function enviar() {
+  var nome = document.getElementById("nomeid");
+  if (nome.value != "nomeid") {
+      alert('Obrigado sr(a) ' + nome.value +
+      ' os seus dados foram enviados com sucesso.');
+
+  }
+
+}
+
